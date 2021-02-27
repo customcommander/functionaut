b6409dfc976d4b0daee6703830dd6c9d0c9c345a
customcommander <hello@spinjs.com>
2021-02-27 21:20:56 +0000

    feat: implement flip

--- src/functions.js
+++ src/functions.js
@@ -66,5 +66,33 @@ module.exports = {
   curry: fn => {
     assert_function(fn, 'curry: `fn` is not a function');
     return _curry(fn);
+  },
+
+  /**
+   * Takes a function `fn` of arity 2 (or more) and returns
+   * a (curried) version of it in which the first two parameters
+   * have been swapped.
+   *
+   * @example
+   * ```javascript
+   * const x = (a, b, c) => a + b + c;
+   * x('foo', 'bar', 'baz');
+   * //=> 'foobarbaz'
+   *
+   * const y = flip(x);
+   * y('foo')('bar', 'baz');
+   * //=> 'barfoobaz'
+   * ```
+   *
+   * @public
+   * @param {function()} fn
+   * @return {function()}
+   */
+  flip: fn => {
+    assert_function(fn, 'flip: `fn` is not a function');
+    return _curry(function (a, b) {
+      const [x, y, ...z] = Array.from(arguments);
+      return fn(y, x, ...z);
+    });
   }
 };
