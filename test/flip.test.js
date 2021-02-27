a8a8b726a6e34a35e413b76a4c00d4029ad18be3
customcommander <hello@spinjs.com>
2021-02-27 21:20:56 +0000

    feat: implement flip

---
+++ test/flip.test.js
@@ -0,0 +1,28 @@
+const test = require('tape');
+const td = require('testdouble');
+const {flip} = require('../dist');
+
+test('flip inverts the order of the first two parameters.', t => {
+  const fn = td.function();
+  const flipped = flip(fn);
+  flipped('foo')('bar');
+  td.verify(fn('bar', 'foo'));
+  flipped('baz', 'bat');
+  td.verify(fn('bat', 'baz'));
+  t.end();
+});
+
+test('flip forwards the rest of the parameters.', t => {
+  const fn = td.function();
+  const flipped = flip(fn);
+  flipped('foo')('bar', 'baz');
+  td.verify(fn('bar', 'foo', 'baz'));
+  flipped('foo', 'bar', 'baz', 'bat');
+  td.verify(fn('bar', 'foo', 'baz', 'bat'));
+  t.end();
+});
+
+test('flip fails when not given a function.', t => {
+  t.throws(() => flip([]));
+  t.end();
+});
