3d445102fd7b1e1528e097eb32fa42d16e3f02b2
customcommander <hello@spinjs.com>
2021-07-06 23:36:56 +0000

    feat: implement cond

--- src/functions.js
+++ src/functions.js
@@ -155,5 +155,62 @@ module.exports = {
    * @param {?} x
    * @returns {function(): ?}
    */
-  constant: x => () => x
+  constant: x => () => x,
+
+  /**
+   * Takes a list of predicate/function pairs and returns a function that takes
+   * any number of arguments and applies them to the predicates until one is
+   * satisfied. At which point the function associated with that predicate
+   * is applied to the same arguments and we take its return value.
+   * Predicates must return true (not truthy). Returns `undefined` when
+   * no predicates are satisfied.
+   *
+   * @example
+   * ```javascript
+   * const record = cond( ({age}) => age < 5,
+   *                        ({name}) => `Free for ${name}.`,
+   * 
+   *                      ({age}) => age < 12,
+   *                        ({name}) => `50% discount for ${name}.`,
+   * 
+   *                      ({age}) => age < 20,
+   *                        ({name}) => `20% discount for ${name}.`,
+   * 
+   *                      ({age}) => age < 65,
+   *                        ({name}) => `Standard charge for ${name}.`,
+   * 
+   *                      ({age}) => age >= 65,
+   *                        ({name}) => `40% discount for ${name}.`);
+   *
+   * record({name: 'Harry', age: 2});
+   * //=> 'Free for Harry.'
+   *
+   * record({name: 'Jane', age: 11});
+   * //=> '50% discount for Jane.'
+   *
+   * record({name: 'Idris', age: 18});
+   * //=> '20% discount for Idris.'
+   *
+   * record({name: 'Bob', age: 40});
+   * //=> 'Standard charge for Bob.'
+   *
+   * record({name: 'John', age: 65});
+   * //=> '40% discount for John.'
+   * ```
+   *
+   * @public
+   * @param  {...function()} fn
+   * @return {function()}
+   * @throws When called with no or a non-even number of arguments or with non-function arguments.
+   */
+  cond: (...fn) => {
+    fn.forEach((f, i) => assert_function(f, `cond: arg at ${i} is not a function.`));
+    if (fn.length === 0) throw new Error('cond: called with no arguments.');
+    if (fn.length % 2 !== 0) throw new Error('cond: called with uneven number of arguments.');
+    return (...args) => {
+      for (let i=0; i<fn.length; i+=2) {
+        if (fn[i](...args) === true) return fn[i+1](...args);
+      }
+    };
+  }
 };
---
+++ test/cond.test.js
@@ -0,0 +1,72 @@
+const td = require('testdouble');
+const test = require('tape');
+const {cond: sut} = require('../dist');
+
+test('cond throws when not called correctly.', t => {
+  t.throws(() => sut(),
+    'throws when called with no arguments.');
+
+  t.throws(() => sut(td.func(), {}),
+    'throws when called with non-function arguments.');
+
+  t.throws(() => sut(td.func()),
+    'throws when called with a non-even number of arguments.');
+
+  t.end();
+});
+
+test('cond returns a function.', t => {
+  t.true(typeof sut(td.func(), td.func()) === 'function');
+  t.end();
+});
+
+test('cond\'ed function skips all functions until a predicate is true then skips the rest.', t => {
+  const pred1 = td.func();
+  td.when(pred1(40, 2)).thenReturn(false);
+
+  const fn1 = td.func();
+  td.when(fn1(40, 2)).thenThrow(new Error('should not have been called'));
+
+  const pred2 = td.func();
+  td.when(pred2(40, 2)).thenReturn(true);
+
+  const fn2 = td.func();
+  td.when(fn2(40, 2)).thenReturn('ok!');
+
+  const pred3 = td.func();
+  const fn3 = td.func();
+
+  const conded = sut(pred1, fn1, pred2, fn2, pred3, fn3);
+
+  t.same(conded(40, 2), 'ok!');
+  td.verify(pred3(), {ignoreExtraArgs: true, times: 0});
+  td.verify(fn3(), {ignoreExtraArgs: true, times: 0});
+
+  t.end();
+});
+
+test('cond\'ed function ignores truthy predicates.', t => {
+  const pred1 = td.func();
+  td.when(pred1(40, 2)).thenReturn(1);
+
+  const fn1 = td.func();
+  td.when(fn1(40, 2)).thenThrow(new Error('should not have been called'));
+
+  const pred2 = td.func();
+  td.when(pred2(40, 2)).thenReturn(true);
+
+  const fn2 = td.func();
+  td.when(fn2(40, 2)).thenReturn('ok!');
+
+  t.same(sut(pred1, fn1, pred2, fn2)(40, 2), 'ok!');
+  t.end();
+});
+
+test('cond\'ed function returns undefined when no predicates are true.', t => {
+  const pred = td.func();
+  const fn = td.func();
+  td.when(pred(40, 2)).thenReturn(false);
+
+  t.same(sut(pred, fn)(40, 2), undefined);
+  t.end();
+});
