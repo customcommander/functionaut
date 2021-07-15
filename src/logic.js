f3d6e579165b38d4941f86ffb3ee345c0b84174b
customcommander <hello@spinjs.com>
2021-07-15 23:23:52 +0000

    feat: implement all

--- src/logic.js
+++ src/logic.js
@@ -3,6 +3,7 @@
  * @copyright (c) 2021 Julien Gonzalez <hello@spinjs.com>
  */
 
+const {assert_function} = require('./private/helpers');
 const {curry} = require('./functions');
 
 /**
@@ -53,5 +54,58 @@ module.exports = {
    * @return {boolean}
    * @see eq
    */
-  ne: curry((a, b) => Object.is(a, b) == false)
+  ne: curry((a, b) => Object.is(a, b) == false),
+
+  /**
+   * Takes a list of predicates and returns a function that takes
+   * any number of arguments and returns true if and only if
+   * all predicates return true for all arguments.
+   * Returns true when called with no arguments.
+   * Predicates must return `true` (not truthy).
+   *
+   * @example
+   * > Check all elements are odd numbers (Vanilla JS):
+   *
+   * ```javascript
+   * const isNum = x => typeof x === 'number';
+   * const isOdd = x => x % 2 !== 0;
+   *
+   * const xs = [1, "3", 5];
+   * const ys = [1, 3, 5];
+   *
+   * xs.every(x => isNum(x) && isOdd(x));
+   * //=> false
+   *
+   * ys.every(x => isNum(x) && isOdd(x));
+   * //=> true
+   * ```
+   *
+   * > With all:
+   *
+   * ```javascript
+   * const oddNumber = all(isNum, isOdd);
+   *
+   * xs.every(x => oddNumber(x));
+   * //=> false
+   *
+   * ys.every(y => oddNumber(y));
+   * //=> true
+   *
+   * oddNumber(...xs);
+   * //=> false
+   *
+   * oddNumber(...ys);
+   * //=> true
+   * ```
+   *
+   * @public
+   * @param  {...function(?): boolean} fn
+   * @returns {function(...?): boolean}
+   * @throws When called with no or non-function predicates.
+   */
+  all: (...fn) => {
+    if (fn.length === 0) throw new Error('all: called with no predicates.');
+    fn.forEach((f, i) => assert_function(f, `all: arg at ${i} is not a function.`));
+    return (...arg) => arg.every(a => fn.every(f => f(a) === true));
+  }
 };
---
+++ test/all.test.js
@@ -0,0 +1,61 @@
+const td = require('testdouble');
+const test = require('tape');
+const {all: sut} = require('..');
+
+test('all throws when called with no predicates', t => {
+  t.throws(() => sut());
+  t.end();
+});
+
+test('all throws when called with non-function arguments', t => {
+  t.throws(() => sut(td.func(), 42, td.func()));
+  t.end();
+});
+
+test('all returns true when there are no arguments to verify', t => {
+  const p = td.func();
+  t.same(sut(p)(), true);
+  td.verify(p(), {ignoreExtraArgs: true, times: 0});
+  t.end();
+});
+
+test('all returns true when all predicates returned true for all arguments', t => {
+  const p1 = td.func();
+  td.when(p1(1)).thenReturn(true);
+  td.when(p1(2)).thenReturn(true);
+  td.when(p1(3)).thenReturn(true);
+
+  const p2 = td.func();
+  td.when(p2(1)).thenReturn(true);
+  td.when(p2(2)).thenReturn(true);
+  td.when(p2(3)).thenReturn(true);
+
+  t.same(sut(p1, p2)(1), true);
+  t.same(sut(p1, p2)(1, 2), true);
+  t.same(sut(p1, p2)(1, 2, 3), true);
+  t.end();
+});
+
+test('all returns false when a predicate returns false for any argument', t => {
+  const p1 = td.func();
+  td.when(p1(1)).thenReturn(true);
+  td.when(p1(2)).thenReturn(true);
+  td.when(p1(3)).thenReturn(false);
+
+  const p2 = td.func();
+  td.when(p2(1)).thenReturn(true);
+  td.when(p2(2)).thenReturn(true);
+  td.when(p2(3)).thenThrow(new Error('all should short-circuit when a predicate returns false'));
+
+  t.same(sut(p1, p2)(1), true);
+  t.same(sut(p1, p2)(1, 2), true);
+  t.same(sut(p1, p2)(1, 2, 3), false);
+  t.end();
+});
+
+test('all only accepts true not truthy', t => {
+  const p1 = td.func();
+  td.when(p1(1)).thenReturn(42);
+  t.same(sut(p1)(1), false);
+  t.end();
+});
