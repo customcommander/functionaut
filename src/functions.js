71f3bd84d7885c7439fc8f346e34a10388da420e
customcommander <hello@spinjs.com>
2021-07-16 11:07:13 +0000

    feat: implement partial

--- src/functions.js
+++ src/functions.js
@@ -239,5 +239,34 @@ module.exports = {
     assert_function(f, 'on: `f` is not a function');
     assert_function(g, 'on: `g` is not a function');
     return _curry((a, b) => f(g(a), g(b)));
-  })
+  }),
+
+  /**
+   * Allows the partial application of a non-curried function.
+   * Takes a function `f` and a list of initial arguments and
+   * returns a new function that takes the remaining arguments and
+   * applies `f` to both lists of arguments.
+   *
+   * @example
+   * ```javascript
+   * const msg = (a, b, c) => `${a} ${b} ${c}`;
+   *
+   * partial(msg, 'Hello')('World', '!');
+   * //=> "Hello World !"
+   *
+   * partial(msg, 'Hello', 'World')('!');
+   * //=> "Hello World !"
+   * ```
+   *
+   * @public
+   * @param {function(...?): ?} f The partially applied function.
+   * @param  {...?} init The initial list of arguments
+   * @returns {function(...?): ?} A function that takes the remaining arguments
+   *                              then applies `f` to both lists of arguments.
+   * @throws When `f` is not a function
+   */
+  partial: (f, ...init) => {
+    assert_function(f, 'partial: `f` is not a function');
+    return (...tail) => f(...init, ...tail);
+  }
 };
---
+++ test/partial.test.js
@@ -0,0 +1,22 @@
+const td = require('testdouble');
+const test = require('tape');
+const {partial: sut} = require('..');
+
+test('partial throws when `f` is not a function', t => {
+  t.throws(() => sut(42));
+  t.end();
+});
+
+test('partial applies f to the initial arguments and the remaining arguments', t => {
+  const f = td.func();
+  td.when(f(10, 20, 5, 5, 1, 1)).thenReturn(42);
+  t.same(sut(f, 10, 20, 5)(5, 1, 1), 42);
+  t.end();
+});
+
+test('partial can be given no arguments at all', t => {
+  const f = td.func();
+  td.when(f()).thenReturn(42);
+  t.same(sut(f)(), 42);
+  t.end();
+});
