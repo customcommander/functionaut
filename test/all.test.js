8f9bc01382a83c7f89d4aca7caae2711f9e171b3
customcommander <hello@spinjs.com>
2021-10-12 22:51:17 +0000

    feat: all checks the element of a list against a predicate. close #24
    BREAKING CHANGE:
    
    The function has been changed to accept only one predicate and a list
    of values instead of any number of parameters.
    
    Migration:
    
    v1: `all(isNum, isOdd)(1, 2, 3)`
    
    v2: `all(x => isNum(x) === true && isOdd(x) === true, [1, 2, 3])`

--- test/all.test.js
+++ test/all.test.js
@@ -2,50 +2,69 @@ const td = require('testdouble');
 const test = require('tape');
 const {all: sut} = require('..');
 
-test('all returns true when there are no arguments to verify', t => {
+test('returns true if xs is empty; does not invoke pred at all', t => {
   const p = td.func();
-  t.same(sut(p)(), true);
-  td.verify(p(), {ignoreExtraArgs: true, times: 0});
+  const expected = [].every(() => true);
+
+  t.same(sut(p)([]), expected);
+  t.same(sut(p)(""), expected);
+  t.same(sut(p)({}), expected);
+
+  td.verify(p(), {times: 0, ignoreExtraArgs: true});
+
   t.end();
 });
 
-test('all returns true when all predicates returned true for all arguments', t => {
-  const p1 = td.func();
-  td.when(p1(1)).thenReturn(true);
-  td.when(p1(2)).thenReturn(true);
-  td.when(p1(3)).thenReturn(true);
+test('returns true if pred is true for all x in xs', t => {
+  const p = td.func();
+  td.when(p(td.matchers.anything())).thenReturn(true);
 
-  const p2 = td.func();
-  td.when(p2(1)).thenReturn(true);
-  td.when(p2(2)).thenReturn(true);
-  td.when(p2(3)).thenReturn(true);
+  t.same(sut(p)([1, 2]), true);
+  t.same(sut(p)({a: 4, b: 5}), true);
+  t.same(sut(p)("🥑🌯"), true);
 
-  t.same(sut(p1, p2)(1), true);
-  t.same(sut(p1, p2)(1, 2), true);
-  t.same(sut(p1, p2)(1, 2, 3), true);
+  td.verify(p(1), {times: 1});
+  td.verify(p(2), {times: 1});
+  td.verify(p(4), {times: 1});
+  td.verify(p(5), {times: 1});
+  td.verify(p("🥑"), {times: 1});
+  td.verify(p("🌯"), {times: 1});
+
   t.end();
 });
 
-test('all returns false when a predicate returns false for any argument', t => {
-  const p1 = td.func();
-  td.when(p1(1)).thenReturn(true);
-  td.when(p1(2)).thenReturn(true);
-  td.when(p1(3)).thenReturn(false);
+test('returns false if pred is false for any x of xs', t => {
+  const p = td.func();
+  td.when(p(td.matchers.anything())).thenReturn(false);
 
-  const p2 = td.func();
-  td.when(p2(1)).thenReturn(true);
-  td.when(p2(2)).thenReturn(true);
-  td.when(p2(3)).thenThrow(new Error('all should short-circuit when a predicate returns false'));
+  t.same(sut(p)([1, 2]), false);
+  t.same(sut(p)({a: 4, b: 5}), false);
+  t.same(sut(p)("🥑🌯"), false);
 
-  t.same(sut(p1, p2)(1), true);
-  t.same(sut(p1, p2)(1, 2), true);
-  t.same(sut(p1, p2)(1, 2, 3), false);
+  td.verify(p(1), {times: 1});
+  td.verify(p(2), {times: 0});
+  td.verify(p(4), {times: 1});
+  td.verify(p(5), {times: 0});
+  td.verify(p("🥑"), {times: 1});
+  td.verify(p("🌯"), {times: 0});
+
   t.end();
 });
 
-test('all only accepts true not truthy', t => {
-  const p1 = td.func();
-  td.when(p1(1)).thenReturn(42);
-  t.same(sut(p1)(1), false);
+test('does not work with truthy predicates', t => {
+  const p = td.func();
+  td.when(p(td.matchers.anything())).thenReturn(1);
+
+  t.same(sut(p)([1, 2]), false);
+  t.same(sut(p)({a: 4, b: 5}), false);
+  t.same(sut(p)("🥑🌯"), false);
+
+  td.verify(p(1), {times: 1});
+  td.verify(p(2), {times: 0});
+  td.verify(p(4), {times: 1});
+  td.verify(p(5), {times: 0});
+  td.verify(p("🥑"), {times: 1});
+  td.verify(p("🌯"), {times: 0});
+
   t.end();
 });
