caf8708f2f21a95e5cfca82447ca70f583c0eed6
customcommander <hello@spinjs.com>
2021-10-16 17:35:22 +0000

    docs: missing space in map example

--- src/map.js
+++ src/map.js
@@ -35,7 +35,7 @@ module.exports = {
    * double([1, 2, 3]);
    * //=> [2, 4, 6]
    *
-   * double({a: 1, b:2, c: 3});
+   * double({a: 1, b: 2, c: 3});
    * //=> {a: 2, b: 4, c: 6}
    *
    * double("🌯🍣🌮");
