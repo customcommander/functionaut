14c0f64e58501022338194ced3f3b261343ea253
customcommander <hello@spinjs.com>
2021-10-28 08:57:48 +0000

    feat: implement anyfn

---
+++ src/anyfn.js
@@ -0,0 +1,37 @@
+/**
+ * @license MIT
+ * @copyright (c) 2021 Julien Gonzalez <hello@spinjs.com>
+ */
+
+/**
+ * @namespace
+ * @alias ROOT
+ */
+module.exports = {
+  /**
+   * Takes one or more predicates and returns a function
+   * that takes any number of arguments and returns `true`
+   * if one predicate passed when applied to these arguments.
+   * Returns `false` if none did.
+   *
+   * @example
+   * > Make sure that both x and y are either numbers or strings.
+   *
+   * ```javascript
+   * const is_num = x => typeof x == 'number';
+   * const is_str = x => typeof x == 'string';
+   *
+   * const num_or_str = anyfn( (x, y) => is_num(x) && is_num(y)
+   *                         , (x, y) => is_str(x) && is_str(y));
+   *
+   * num_or_str(  40,    2); //=> true
+   * num_or_str('21', '21'); //=> true
+   * num_or_str('40',    2); //=> false
+   * ```
+   *
+   * @public
+   * @param {...function(...?): boolean} fn One or more predicates
+   * @returns {function(...?): boolean}
+   */
+  anyfn: (...fn) => (...args) => fn.some(f => f(...args) === true)
+};
