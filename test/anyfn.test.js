fd8f61885c7988d06c316f5b116fddb80624f166
customcommander <hello@spinjs.com>
2021-12-30 14:20:33 +0000

    feat: anyfn: functions can return logical true
    BREAKING CHANGE: anyfn(...fn)(...args): functions can return logical true

--- test/anyfn.test.js
+++ test/anyfn.test.js
@@ -2,35 +2,36 @@ const test = require('tape');
 const td = require('testdouble');
 const {anyfn: sut} = require('..');
 
-test('anyfn(...fn)(...args) -> true when one predicate passed', t => {
-  const a = Symbol();
-  const b = Symbol();
+test('anyfn(...fn)(...args)', t => {
 
-  const f = td.func();
-  const g = td.func();
-  const h = td.func();
+  t.test('true when any fn returned logical true when applied to the arguments', st => {
+    const a = Symbol();
+    const b = Symbol();
+    const f = td.func();
+    const g = td.func();
+    const h = td.func();
 
-  td.when(f(a, b)).thenReturn(true);
-  td.when(g(a, b)).thenDo(() => t.fail('`g(a, b)` called but `f(a, b)` passed'));
-  td.when(h(a, b)).thenDo(() => t.fail('`h(a, b)` called but `f(a, b)` passed'));
+    td.when(f(a, b)).thenDo(() => (st.pass('f(a, b) returned logical false'), false));
+    td.when(g(a, b)).thenDo(() => (st.pass('g(a, b) returned logical true'), 0));
 
-  t.same(sut(f, g, h)(a, b), true);
+    st.plan(4)
+    st.true(sut(f, g, h)(a, b) === true);
+    st.true(td.explain(h).callCount === 0);
+    st.end();
+  });
 
-  td.verify(f(), {times:1, ignoreExtraArgs: true});
+  t.test('false when no fn returned logical true when applied to the arguments', st => {
+    const a = Symbol();
+    const b = Symbol();
+    const f = td.func();
 
-  t.end();
-});
+    td.when(f(a, b)).thenDo(() => (st.pass('f(a, b) called ✓'), false));
 
-test('anyfn(...fn)(...args) -> false when no predicates passed', t => {
-  const a = Symbol();
-  const b = Symbol();
-
-  const f = td.func();
-  td.when(f(a, b)).thenReturn(1);
-
-  t.same(sut(f, f, f)(a, b), false);
-
-  td.verify(f(), {times:3, ignoreExtraArgs: true});
+    st.plan(5);
+    st.true(sut(f, f, f)(a, b) === false);
+    st.true(td.explain(f).callCount === 3);
+    st.end();
+  });
 
   t.end();
 });
